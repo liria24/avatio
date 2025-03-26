07530066101c9ba60e5b70aabce7283f48801524
Liry24 <47878070+Liry24@users.noreply.github.com>
2025-03-26 10:55:19 +0900

    fix: ai moderation

--- server/api/check/ai-moderation/setup.ts
+++ server/api/check/ai-moderation/setup.ts
@@ -3,6 +3,9 @@ import { createOpenAI } from '@ai-sdk/openai';
 import { z } from 'zod';
 
 export default defineEventHandler(async (event) => {
+    // 精度とコストに難があるためまだ運用しません
+    return null;
+
     const { id } = await getQuery<{ id: number }>(event);
 
     const { data } = await event.$fetch('/api/setup', {
@@ -18,9 +21,6 @@ export default defineEventHandler(async (event) => {
         userName: data.author.name,
         tags: data.tags,
         unityVersion: data.unity,
-        images: data.images.map(
-            (image) => `https://images.avatio.me/setup/${image.name}`
-        ),
         items: Object.values(data.items)
             .flat()
             .map((item) => {
@@ -31,13 +31,31 @@ export default defineEventHandler(async (event) => {
             }),
     };
 
+    const content = [
+        {
+            type: 'text',
+            text: JSON.stringify(setupData).toString().replace(/\s+/g, ' '),
+        },
+        ...data.images.map((image) => ({
+            type: 'image',
+            image: new URL(`https://images.avatio.me/setup/${image.name}`),
+        })),
+    ];
+
     const system = `
         あなたはwebサービスの投稿データをパトロールするAIです
         このサービスは、VRSNSで使用するアバターに使われる販売アセットをまとめて投稿するものです
         提供された内容を解析して、投稿に問題があるか判断してください
 
-        # 投稿データのスキーマ
+        # 問題のある投稿の例
 
+        - 他人を不快にする可能性のある言葉遣いが含まれる
+        - VRSNSアバターを共有するサービスにも関わらず、関係のない画像（風景のみや実写画像など）が含まれている
+        - 局部を露出している画像が含まれている
+
+        # 投稿データ
+
+        ## JSON
         {
             id: number, // 投稿のID
             title: string, // 投稿のタイトル
@@ -45,37 +63,25 @@ export default defineEventHandler(async (event) => {
             userName: string, // 投稿者の名前
             tags: string[], // 投稿のタグ
             unityVersion: string, // 投稿データのUnityバージョン番号
-            images: string[], // 投稿に含まれる画像のファイル名
             items: {
                 shapekeys: string[], // 投稿に登録されたアイテムのシェイプキー名称
                 note: string, // 投稿に登録されたアイテムの付加ノート
             }[] // 投稿に含まれるアイテムの情報
         }
 
+        ## 画像
+        - 添付された画像です
+
         # レスポンス内容
 
-        - id: 投稿のIDを記入してください
-        - issues: 投稿の問題点を問題の数だけ記入してください
-            - severity: 問題の深刻度を表す文字列です
-            - category: 問題のカテゴリを表す文字列です
-            - reason: 何故問題があると判定したのか、理由を具体的に表現してください
-
-        # 問題があるかどうかの判定要素
-
-        - スパム・荒らし目的
-        - 過度に性的・暴力的な内容
-
-        - 画像が使われていないことは問題ではありません
-        - 説明文が未記入であることは問題ではありません
-        - タグが未記入であることは問題ではありません
-        - シェイプキーのリストが空であることは問題ではありません
-        - アイテムのノートが未記入であることは問題ではありません
-        - Unityバージョンが未指定であることは問題ではありません
+        - severity: 問題の深刻度を表す文字列です
+        - category: 問題のカテゴリを表す文字列です
+        - reason: 何故問題があると判定したのか、理由を具体的に表現してください
 
         # 注意
 
         - 日本語で出力してください
-        - 明らかに内容に違和感のあるもののみを報告してください
+        - severityが高いもののみを出力してください
         - 投稿に問題がない場合、issuesは空配列を返してください
     `;
 
@@ -88,9 +94,8 @@ export default defineEventHandler(async (event) => {
 
     try {
         const { object } = await generateObject({
-            model: openai('o3-mini', { structuredOutputs: true }),
+            model: openai('gpt-4o-mini', { structuredOutputs: true }),
             schema: z.object({
-                id: z.number(),
                 issues: z.array(
                     z.object({
                         severity: z.string(),
@@ -100,7 +105,12 @@ export default defineEventHandler(async (event) => {
                 ),
             }),
             system: system.trim().replace(/\s+/g, ' '),
-            prompt: JSON.stringify(setupData).replace(/\s+/g, ' '),
+            messages: [
+                {
+                    role: 'user',
+                    content: content as never,
+                },
+            ],
         });
         return object;
     } catch (error) {
