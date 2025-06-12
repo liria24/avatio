efd44bdb9352ff4509f3d925ef8229ecb255201f
Liry24 <47878070+Liry24@users.noreply.github.com>
2025-06-12 13:57:27 +0900

    feat: remove unused admin report API handler

--- server/api/test.ts
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler(async () => {
-    return $fetch('/api/admin/report', {
-        method: 'POST',
-        headers: {
-            authorization: `Bearer ${useRuntimeConfig().adminKey}`,
-        },
-    })
-})
