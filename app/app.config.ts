d97a225b7eb5aaac97b218dde2a341046d2655cb
Liry24 <liry241999@gmail.com>
2025-11-05 20:12:26 +0900

    chore: bump version to 1.2.7

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
     app: {
         name: 'Avatio',
-        version: '1.2.6',
+        version: '1.2.7',
         site: 'https://avatio.me',
         repo: 'https://github.com/liria24/avatio',
         githubIssue: 'https://github.com/liria24/avatio/issues/new/choose',
