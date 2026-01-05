2321cf3b1d6dbd8673efa95233844d3ef704f307
Liry24 <liry241999@gmail.com>
2026-01-05 14:20:45 +0900

    chore: upgrade vite to 8.0.0-beta.5 and disable vue options api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,6 +43,11 @@ export default defineNuxtConfig({
     css: ['~/assets/css/main.css'],
 
     vite: {
+        vue: {
+            features: {
+                optionsAPI: false,
+            },
+        },
         optimizeDeps: {
             include: [
                 'prosemirror-state',
