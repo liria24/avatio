4f69bf1343e422c3b22b1013ad88d5b2d866e228
Liry24 <liry241999@gmail.com>
2026-02-17 15:55:56 +0900

    chore: release v1.7.0

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
     app: {
         name: 'Avatio',
-        version: '1.6.2',
+        version: '1.7.0',
         site: 'https://avatio.me',
         repo: 'https://github.com/liria24/avatio',
         githubIssue: 'https://github.com/liria24/avatio/issues/new/choose',
