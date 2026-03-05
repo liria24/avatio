9cfaaf5cbe351913b48004b8fb0525db9b2e11e0
Liry24 <liry241999@gmail.com>
2026-03-05 11:39:02 +0900

    refactor: organize notification components

--- app/components/notification/popover.vue
+++ app/components/notification/popover.vue
@@ -37,7 +37,7 @@ const { all, status } = useNotifications()
                 </p>
 
                 <div v-else class="flex flex-col gap-2">
-                    <LazyUserNotification
+                    <LazyNotificationItem
                         v-for="notification in all"
                         :key="notification.id"
                         :notification
