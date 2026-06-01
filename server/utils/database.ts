06494bcd9de235e4645db880406e558a5908119a
Liry24 <liry241999@gmail.com>
2026-06-01 13:07:46 +0900

    refactor: remove commented-out db initialization in database utility

--- server/utils/database.ts
+++ server/utils/database.ts
@@ -4,7 +4,6 @@ import { drizzle } from 'drizzle-orm/neon-serverless'
 import { relations } from '../../database/relations'
 import * as schema from '../../database/schema'
 
-// const db = drizzle({ client, relations })
 const useDB = () =>
     drizzle({
         client: new Pool({ connectionString: process.env.NEON_DATABASE_URL }),
